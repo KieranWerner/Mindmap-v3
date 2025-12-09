c1b76263451f9391b70b576760036c0fee808f2c
KieranWerner <149817738+KieranWerner@users.noreply.github.com>
2025-12-09 16:47:22 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // Setzen Sie dies auf './' oder entfernen Sie die Zeile für lokale Tests
-  base: './', 
+  base: './', // Sorgt für relative Pfade, gut für flexible Deployments
+  build: {
+    outDir: 'docs', // Ändert den Output-Ordner von 'dist' zu 'docs'
+    emptyOutDir: true, // Leert den Ordner vor dem Bauen
+  }
 })
